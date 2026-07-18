289ce7f6741f3a63100bb3b20b927e8967ddb381
it's_manish <geekycodermanish@gmail.com>
2026-07-18 17:16:06 +0530

    worm up section...

---
+++ script.js
@@ -0,0 +1,153 @@
+// let voters = [
+//     {name: "manish", age: 27},
+//     {name: "Rohit", age: 12},
+//     {name: "Ajay", age: 23},
+//     {name: "Rahul", age: 45},
+//     {name: "Nikhil", age: 13},
+//     {name: "Raj", age: 18},
+//     {name: "Yesh", age: 19}
+// ]
+
+// let eleagbleVoters = []
+// let index;
+
+// function checkVoter (){
+//     console.log(voters[1])
+
+//     // for(let key of voters){
+//     //     if(key.age >= 18){
+//     //         eleagbleVoters.push(key)
+//     //     }
+//     // }
+
+//     // return eleagbleVoters
+
+// }
+// checkVoter()
+// // console.log(eleagbleVoters)
+// // // console.log()
+
+
+
+// const arr = [10, 20, 30, 2, 50];
+
+// let result;
+
+// function check(){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] < arr[i + 1]){
+//             result = arr[i]
+//         }
+//     }
+
+//     return console.log(result)
+// }
+
+// check()
+
+
+
+// const arr = [10, 20, 30, 2, 50];
+
+// function searchElement (arr, num) {
+//   if(!num && !arr) return -1 ;
+//     for(let i = 0; i < arr.length; i++){
+//       if(arr[i] == num){
+//         return i;
+//       }
+//     }
+//     return -1
+// }
+
+// console.log(searchElement(arr) )
+
+
+// const arr = [10, 20, -30, 2, -50, 1, 3, -9];
+
+// let describe = 0 // 1
+
+// function check (arr, describe){
+//   if(!arr || !describe) return 0;
+//   let result = 0
+//   for(let i = 0; i < arr.length; i++){
+//       if(describe === 0){
+//         if(arr[i] < 0){
+//           result++
+//         }
+//       }else {
+//         if(arr[i] > 0){
+//           result++
+//         };
+//       }
+
+//   }
+
+//   return result;
+// }
+
+
+// console.log(check(arr, 1))
+
+
+
+// const arr = [80, 10, 20, 30, 2, -50, 1, 3, -9];
+
+// function check (arr, desc) {
+//   if(!arr || !desc) return 0
+  
+//   let result = 0;
+//   for(let i = 0; i < arr.length; i++){
+//     if(desc == 1){
+//       if(result < arr[i]){
+//         result = arr[i]
+//       }
+//     }else {
+//       if(result > arr[i]){
+//         result = arr[i]
+//       }
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(check(arr, 0))
+
+
+
+// let arr = [10, 20, 4, 30, 2, -50, 1, 3, -9];
+
+// function check(arr) {
+
+//   if(!arr && arr.length < 2) return null
+//   let max = 0;
+//   let secondMax = 0;
+
+//   for(let i = 0; i < arr.length; i++){
+//     if(max < arr[i]){
+//       secondMax = max
+//       max = arr[i]
+//     }
+//     // else if(secondMax < arr[i]){
+//     //   secondMax = arr[i]
+//     // }
+//   }
+
+
+//   return secondMax;
+// }
+
+
+// console.log(check(arr))
+
+
+
+
+for(let outer = 0; outer < 5; outer++){
+    for(let inner = outer; inner > 0; inner--){
+      console.log("inner....", outer, inner)
+      console.log('--------------------------------')
+      // console.log("*")
+    }
+    // console.log("*")
+}
